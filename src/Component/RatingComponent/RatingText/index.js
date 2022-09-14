5a5b7e33d15436669b8120cac6d0f85992f23fce
Ada <byjean94@gmail.com>
2022-09-14 21:50:23 +0200

    Add Overpass Font

--- src/Component/RatingComponent/RatingText/index.js
+++ src/Component/RatingComponent/RatingText/index.js
@@ -1,7 +1,7 @@
 import { Text } from "@chakra-ui/react"
 
 export function RatingText() {
-    return <Text color="#959EAC" fontSize="15px">
+    return <Text color="#959EAC" fontSize="15px" fontFamily="Overpass" fontWeight="400">
         Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
     </Text>
 }
