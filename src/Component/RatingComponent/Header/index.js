0cb06cf8915e2329831023c4699226a253630e49
Ada <byjean94@gmail.com>
2022-09-14 21:20:46 +0200

    Add Font Sizing and Border Radius to Rating Component

--- src/Component/RatingComponent/Header/index.js
+++ src/Component/RatingComponent/Header/index.js
@@ -1,5 +1,5 @@
 import { Heading } from "@chakra-ui/react"
 
 export function Header() {
-    return <Heading color="white">How did we do?</Heading>
+    return <Heading color="white" fontSize="22px">How did we do?</Heading>
 }
