4615ae2bd3452c3267441ff32e0998fb1580a97a
Ada <byjean94@gmail.com>
2022-09-14 20:35:29 +0200

    Add Colors to Rating Component

--- src/Component/RatingComponent/RatingText/index.js
+++ src/Component/RatingComponent/RatingText/index.js
@@ -1,5 +1,7 @@
 import { Text } from "@chakra-ui/react"
 
 export function RatingText() {
-    return <Text>Rate us</Text>
+    return <Text color="#959EAC" fontSize="15px">
+        Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+    </Text>
 }
