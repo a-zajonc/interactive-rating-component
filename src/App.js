edb8885bc74e21aac96142f03ec703c88957cab8
Ada <byjean94@gmail.com>
2022-09-14 19:17:06 +0200

    Add Background Color and Website Height

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Container } from '@chakra-ui/react'
 import { RatingComponent } from './Component/RatingComponent';
 
 function App() {
   return <ChakraProvider>
-    <RatingComponent />
+    <Container bgColor="#1C1E21" height="100vh" display='flex' justifyContent='center' alignItems='center'>
+      <RatingComponent />
+    </Container>
   </ChakraProvider>
 }
 
