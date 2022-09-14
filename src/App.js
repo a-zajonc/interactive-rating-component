95be844ce3541d937a90a831c4ac30ba103c4953
Ada <byjean94@gmail.com>
2022-09-14 17:07:24 +0200

    Install Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
-  return (
+  return <ChakraProvider>
     <div className="App">
+      Hello
     </div>
-  );
+  </ChakraProvider>
 }
 
 export default App;
