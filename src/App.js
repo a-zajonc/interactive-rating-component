a138dda2de83f4aaffa1b988bc2fb4274e7002b4
Ada <byjean94@gmail.com>
2022-09-15 12:30:44 +0200

    Add Route to Rating and Thank You Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { ChakraProvider, Box } from '@chakra-ui/react'
 import { RatingComponent } from './Component/RatingComponent';
 import { ThankYouComponent } from './Component/ThankYouComponent';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return <ChakraProvider>
-    <Box bgColor="#121417" height="100vh" display='flex' justifyContent='center' alignItems='center'>
-      {/* <RatingComponent /> */}
-      <ThankYouComponent />
-    </Box>
+    <Router>
+      <Box bgColor="#121417" height="100vh" display='flex' justifyContent='center' alignItems='center'>
+        <Routes>
+          <Route exact path="/" element={<RatingComponent />} />
+          <Route exact path="/thankyou" element={<ThankYouComponent />} />
+        </Routes>
+      </Box>
+    </Router>
   </ChakraProvider>
 }
 
