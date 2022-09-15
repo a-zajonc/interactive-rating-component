46a3bab702ac409a10e80fbcdaddbc07fefcd61a
Ada <byjean94@gmail.com>
2022-09-15 12:18:20 +0200

    Add CSS in Thank You Component

--- src/Component/ThankYouComponent/ThankYouText/index.js
+++ src/Component/ThankYouComponent/ThankYouText/index.js
@@ -1,7 +1,7 @@
 import { Text } from "@chakra-ui/react";
 
 export function ThankYouText() {
-    return <Text color="#959EAC" fontSize="15px" fontFamily="Overpass" fontWeight="400">
+    return <Text color="#959EAC" fontSize="15px" fontFamily="Overpass" fontWeight="400" margin="7px" marginTop="0px">
         We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!
     </Text>
 }
